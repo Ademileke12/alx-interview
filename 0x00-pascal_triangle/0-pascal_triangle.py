ba0ab3a2283422e43357818b1b2647accc67daa3
Ademileke <38130432+Ademileke12@users.noreply.github.com>
2023-03-22 20:36:51 +0100

    pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+    0-pascal_triangle.py: pascal_triangle()
+"""
+
+
 def pascal_triangle(n):
+    """
+        returns a lis of lists of integers
+        Args:
+            n (int): number of lists and digits
+        Returns: list of lists
+    """
     if n <= 0:
         return []
 
