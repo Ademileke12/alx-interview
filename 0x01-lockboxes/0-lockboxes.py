1fe39c5987f61609c129fe30aab589f7f1e2515e
Ademileke12 <ademilekeabudu@gmail.com>
2023-03-30 14:39:52 +0000

    0-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,23 +2,23 @@
 def canUnlockAll(boxes):
     # Initialize a list to keep track of the boxes that we have opened
     opened_boxes = [False] * len(boxes)
-    opened_boxes[0] = True # the first box is already opened
-    
+    opened_boxes[0] = True
+
     # Initialize a list to keep track of the keys that we have collected
     key_ring = boxes[0]
-    
-    # Keep trying to open more boxes as long as we have new keys
+
+    # keep trying to open more boxes as long as we have new keys
     while key_ring:
         # Get the next key from the key ring
         key = key_ring.pop(0)
-        
+
         # Check if the key opens a new box
         if not opened_boxes[key]:
             # Add the new key to our key ring
             key_ring.extend(boxes[key])
-            
+
             # Mark the new box as opened
             opened_boxes[key] = True
-    
-    # Check if all boxes have been opened
+
+    # check if all boxes have been opened
     return all(opened_boxes)
