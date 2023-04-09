562c3dbdd870dc9b91c94579da8427f015326d3a
Ademileke12 <ademilekeabudu@gmail.com>
2023-04-09 13:25:42 +0000

    0-minoperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+A method that calculates the fewest number of operations needed to result
+in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
+    """
+    For each i > 1, we can calculate dp[i] by iterating over all j < i
+    and checking if we can form i by copying dp[j] characters
+    and pasting them i-j times.
+    If this is possible,
+    then we can update dp[i] to dp[j] + i/j.
+    We take the minimum value of all such dp[j] and update dp[i] to that value.
+    """
+    dp = [float('inf')] * (n+1)
+    dp[1] = 0
+    for i in range(2, n+1):
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i//j)
+    if dp[n] == float('inf'):
+        return 0
+    else:
+        return dp[n]
